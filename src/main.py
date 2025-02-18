09468e187a52e3d7ab12d18a48bbdf8148824d78
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-02-18 21:57:19 +0330

    Initiate Data layer

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from src.data.migration import create_db_and_tables
 from src.presentation import core_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    create_db_and_tables() # FIXME move this to deployment
     yield
 
 
