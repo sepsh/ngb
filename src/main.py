2e30f36ad67b8be6ea45eb97ca644bfb47d2d929
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-02-20 18:56:42 +0330

    Fix CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.data.migration import create_db_and_tables
 from src.presentation import core_router
@@ -8,10 +9,18 @@ from src.presentation import core_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_db_and_tables() # FIXME move this to deployment
+    create_db_and_tables()  # FIXME move this to deployment
     yield
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=core_router)
