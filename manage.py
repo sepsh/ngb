3930821e34ae88cdd084cbe1daa8be013141649e
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-02-20 18:18:51 +0330

    Enable reload

--- manage.py
+++ manage.py
@@ -3,4 +3,4 @@ import uvicorn
 from src.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=6969, log_level="info")
+    uvicorn.run(app=app, host="0.0.0.0", port=6969, log_level="info", reload=True)
